5140e509f1d0592990698ded30218dbae7802895
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-07-17 07:42:18 +0000

    修正 introduction validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, presence: true, length: { maximum: 50 }
+  validates :introduction, length: { maximum: 50 }
 
   def get_profile_image(size)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/_no_image.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/png')
     end
     profile_image.variant(resize: size).processed
